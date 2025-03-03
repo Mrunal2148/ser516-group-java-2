87c2bede6b9d7009a4025746f52698dfb58b48f9
Mrunal2148 <mkapure@asu.edu>
2025-03-02 19:47:33 -0700

    reduced the height of the header TG-122

--- app/frontend/src/components/Header.js
+++ app/frontend/src/components/Header.js
@@ -16,14 +16,14 @@ const Header = () => {
       display: "flex", 
       alignItems: "center", 
       justifyContent: "space-between", 
-      padding: "10px 20px", 
+      padding: "5px 20px", 
       borderBottom: "2px solid #ccc"
     }}>
 
       <img src={asuLogo} alt="ASU Logo" style={{ height: "70px", width: "auto" }} />
 
 
-      <h1 style={{ margin: 0,padding: "20px 0px", textAlign: "center", flex: 1 }}>Project Metrics Calculator</h1>
+      <h1 style={{ margin: 0,padding: "5px 0px", textAlign: "center", flex: 1 }}>Project Metrics Calculator</h1>
 
 
       {version && (
