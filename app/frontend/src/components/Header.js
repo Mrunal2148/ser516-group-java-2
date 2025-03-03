d6112faec81cb16c08bc32d7a3d3ece9a14e6cd9
Shreya Prakash <spraka52@asu.edu>
2025-03-02 19:40:30 -0700

    TG-122 - Added asu logo and release version to the header

--- app/frontend/src/components/Header.js
+++ app/frontend/src/components/Header.js
@@ -1,9 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import asuLogo from '../components/css/ASU-logo.png'; 
 
 const Header = () => {
+  const [version, setVersion] = useState("");
+
+  useEffect(() => {
+    fetch('/version.txt') 
+      .then(response => response.text())
+      .then(data => setVersion(data.trim())) 
+      .catch(error => console.error("Error loading version:", error));
+  }, []);
+
   return (
-    <header className="header">
-      <h1>Project Metrics Calculator</h1>
+    <header className="header" style={{ 
+      display: "flex", 
+      alignItems: "center", 
+      justifyContent: "space-between", 
+      padding: "10px 20px", 
+      borderBottom: "2px solid #ccc"
+    }}>
+
+      <img src={asuLogo} alt="ASU Logo" style={{ height: "70px", width: "auto" }} />
+
+
+      <h1 style={{ margin: 0,padding: "20px 0px", textAlign: "center", flex: 1 }}>Project Metrics Calculator</h1>
+
+
+      {version && (
+        <p style={{ fontSize: "14px", fontWeight: "bold", margin: 0 }}>
+          Version: {version}
+        </p>
+      )}
     </header>
   );
 };
